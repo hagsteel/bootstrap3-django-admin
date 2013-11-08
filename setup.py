9c6adffc409f466f2e63580e7029932e3843e5bb
Jonas Hagstedt <hagstedt@gmail.com>
2013-11-08 15:57:50 +0100

    updated padding

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="admin_bootstrap3",
-    version="0.1.11",
+    version="0.1.12",
     author="Jonas Hagstedt",
     author_email="hagstedt@gmail.com",
     description=("Bootstrap3 admin"),
